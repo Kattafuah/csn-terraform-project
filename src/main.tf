2374edc95664986ffe2fa4d9285870052895c4b6
kattafuah <kattafuah@gmail.com>
2024-11-19 16:51:00 +0000

    staging the creation of modules

--- src/main.tf
+++ src/main.tf
@@ -27,39 +27,39 @@ module "security" {
 #   # name     = var.domain_name
 # }
 
-# # module "rds" {
-# #   source               = "./modules/rds"
-# #   # subnets              = module.vpc.pri_subnets
-# #   # rds_security_group_id = module.security.rds_security_group_id
-# #   # database_username          = module.rds.aws_ssm_parameter.database_username
-# #   # db_password          = module.rds.aws_ssm_parameter.database_password.value
-# #   # vpc_id               = module.vpc.vpc_id
-# # }
-
 module "rds" {
-  source = "./modules/rds"
-  
-  # Pass the required variables to the RDS module
-  rds_identifier                      = var.rds_identifier
-  rds_db_name                         = var.rds_db_name
-  database_username                   = var.database_username
-  instance_class                      = var.instance_class
-  iam_database_authentication_enabled = var.iam_database_authentication_enabled
-  pub_access                          = var.pub_access
+  source               = "./modules/rds"
+  # subnets              = module.vpc.pri_subnets
+  # rds_security_group_id = module.security.rds_security_group_id
+  # database_username          = module.rds.aws_ssm_parameter.database_username
+  # db_password          = module.rds.aws_ssm_parameter.database_password.value
+  # vpc_id               = module.vpc.vpc_id
 }
 
-# module "ecs-fargate" {
-#   source                      = "./modules/ecs-fargate"
-#   # cluster_name                = var.cluster_name
-#   # subnets                     = module.vpc.public_subnets
-#   # security_group_id           = module.security.ecs_security_group_id
-#   execution_role_arn          = var.execution_role_arn
-#   ssm_db_host_param           = module.rds.aws_db_instance.csntp_rds
-#   ssm_db_user_param           = module.rds.database_username
-#   secrets_manager_db_password = module.rds.database_password
-#   rds_identifier              = var.rds_identifier  # Add this line
-#   instance_class              = var.instance_class  # Add this line
-#   iam_database_authentication_enabled = var.iam_database_authentication_enabled  # Add this line
+# module "rds" {
+#   source = "./modules/rds"
+  
+#   # Pass the required variables to the RDS module
+#   rds_identifier                      = var.rds_identifier
+#   rds_db_name                         = var.rds_db_name
+#   database_username                   = var.database_username
+#   instance_class                      = var.instance_class
+#   iam_database_authentication_enabled = var.iam_database_authentication_enabled
+#   pub_access                          = var.pub_access
 # }
+
+module "ecs-fargate" {
+  source                      = "./modules/ecs-fargate"
+  # cluster_name                = var.cluster_name
+  # subnets                     = module.vpc.public_subnets
+  # security_group_id           = module.security.ecs_security_group_id
+  execution_role_arn          = var.execution_role_arn
+  ssm_db_host_param           = module.rds.aws_db_instance.csntp_rds
+  ssm_db_user_param           = module.rds.database_username
+  secrets_manager_db_password = module.rds.database_password
+  rds_identifier              = var.rds_identifier  # Add this line
+  instance_class              = var.instance_class  # Add this line
+  iam_database_authentication_enabled = var.iam_database_authentication_enabled  # Add this line
+}
 
 
