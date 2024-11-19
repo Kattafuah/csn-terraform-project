032e593e9100db57f39b2d9293b85acded0fae52
kattafuah <kattafuah@gmail.com>
2024-11-19 18:13:30 +0000

    redoing modules

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.csntp.id
+  value = module.vpc.aws_vpc.csntp.id
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.wordpress_db.endpoint
+  value = module.ecs-fargate.aws_db_instance.wordpress_db.endpoint
 }
 
 output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
+  value = module.vpc.aws_lb.alb.dns_name
 }
