8d809752f3f727f8b227224700b1aa39a4a24a97
kattafuah <kattafuah@gmail.com>
2024-11-21 12:17:41 +0000

    redoing modules

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,12 +1,12 @@
-output "vpc_id" {
-  value = module.vpc.aws_vpc.csntp.id
-}
+# output "vpc_id" {
+#   value = module.vpc.aws_vpc.csntp.id
+# }
 
-output "rds_endpoint" {
-  value = module.ecs-fargate.aws_db_instance.wordpress_db.endpoint
-}
+# output "rds_endpoint" {
+#   value = module.ecs-fargate.aws_db_instance.wordpress_db.endpoint
+# }
 
-output "alb_dns_name" {
-  value = module.vpc.aws_lb.csntp_elb.dns_name
-}
+# output "alb_dns_name" {
+#   value = module.vpc.aws_lb.csntp_elb.dns_name
+# }
 
