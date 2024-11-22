e4940609110cb441991aaf9fc35caf77dba75ae5
kattafuah <kattafuah@gmail.com>
2024-11-22 08:55:12 +0000

    redoing stuff

--- src/modules/ecs-fargate/outputs.tf
+++ src/modules/ecs-fargate/outputs.tf
@@ -65,9 +65,9 @@ output "rds_endpoint" {
   value = aws_db_instance.csntp_rds.endpoint  # Ensure this resource is declared in the module
 }
 
-output "rds_instance_id" {
-  value = aws_db_instance.csntp_rds.id
-}
+# output "rds_instance_id" {
+#   value = aws_db_instance.csntp_rds.id
+# }
 
 output "rds_db_name" {
   description = "The database name"
